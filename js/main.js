f5803f5fa62ca25f07968c5d191fe4165cd2d1ee
Mike Reinstein <reinstein.mike@gmail.com>
2013-07-29 16:12:18 -0700

    added some more information to the main file #7

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ $.webshims.setOptions({basePath: '/js/lib/shims/'});
 $.webshims.setOptions('forms', {addValidators: true});
 $.webshims.polyfill('forms es5');
 
+/* 
+if you're just shimming form support, it's probably not necessary to wait 
+until the document is ready to bootstrap your app, since it's unlikely most
+users would submit the form before webshims finishes loading. However if you
+are using another feature that must be present when the app starts (e.g., 
+geolocation) you'll probably want to use a document ready event handler:
+
+$(function () {
+	
+});
+*/
+
