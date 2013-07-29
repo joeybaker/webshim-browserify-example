1d9bfedc5808c207ee240e960a9bec17cd0e7f1c
Joey Baker <joey@byjoeybaker.com>
2013-07-29 14:59:06 -0700

    Add note about original polyfiller

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $ = require('jquery')
 
 require('modernizr')
-require('./lib/polyfiller.js')
+require('./lib/polyfiller.js') // you could also point to 'bower_components/webshim/src/polyfiller.js' if you want to use the default
 
 // polyfill forms for old browsers
 $.webshims.setOptions({basePath: '/js/lib/shims/'});
