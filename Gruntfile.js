7aa68ec38a6f432813ecf176322c73dc0531f562
Joey Baker <joey@byjoeybaker.com>
2013-07-30 12:58:15 -0700

    Add comment to explain the more complex cpShims command

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,30 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		browserify: {
-	    main: {
+  grunt.initConfig({
+    browserify: {
+      main: {
         src: './js/main.js'
-	      , dest: './public/js/main.js'
-	      , options: {
-	         debug: true
-	         , shim: {
-	           jquery: {
-	             path: './bower_components/jquery/jquery.js'
-	             , exports: '$'
-	           }
-	           , modernizr: {
-	              path: './bower_components/webshim/demos/js-webshim/dev/extras/modernizr-custom.js'
-	              , exports: 'Modernizr'
-	           }
-	         }
+        , dest: './public/js/main.js'
+        , options: {
+          debug: true
+          , shim: {
+            jquery: {
+              path: './bower_components/jquery/jquery.js'
+              , exports: '$'
+            }
+            , modernizr: {
+               path: './bower_components/webshim/demos/js-webshim/dev/extras/modernizr-custom.js'
+               , exports: 'Modernizr'
+            }
+          }
         }
-	    }
+      }
     }
     , shell: {
+      // copy the shims from bower components to a publicly accessible directory
+      // where they can be loaded async by the webshims polyfiller.js
       cpShims: {
-        command: 'rm -rf ' + path.join(path.dirname('<%= browserify.main.dest %>'), '/lib/shims') + '&& cp -r ' + path.join(components, '/webshim/demos/js-webshim/dev/shims') + path.join(path.dirname('<%= browserify.main.dest %>'), '/lib/')
+        command: 'rm -rf ' + path.join(path.dirname('<%= browserify.main.dest %>'), '/lib/shims')
+          + '&& cp -r ' + path.join(components, '/webshim/demos/js-webshim/dev/shims') + path.join(path.dirname('<%= browserify.main.dest %>'), '/lib/')
       }
     }
   });
